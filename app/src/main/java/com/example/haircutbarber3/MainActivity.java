414f4a44176d923f3a1df8a8cbfcd2905dccd185
alexros <alejandro.de.ros@cieep.com>
2023-03-22 12:39:00 +0100

    bottom nav color correct

--- app/src/main/java/com/example/haircutbarber3/MainActivity.java
+++ app/src/main/java/com/example/haircutbarber3/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.haircutbarber3;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.haircutbarber3.ui.Detalles.DetallesFragment;
@@ -20,10 +25,20 @@ import com.example.haircutbarber3.ui.Servicios.CortesFragment;
 import com.example.haircutbarber3.ui.Servicios.ProductosFragment;
 import com.example.haircutbarber3.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity  {
 
     private ActivityMainBinding binding;
+
+
+    private FirebaseDatabase database;
+    private DatabaseReference refCitas;
+    private FirebaseUser user;
+    private FirebaseAuth userAuth;
 
 
     @Override
@@ -32,6 +47,18 @@ public class MainActivity extends AppCompatActivity  {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //Firebase config
+        //userAuth=FirebaseAuth.getInstance();
+
+        //user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //database = FirebaseDatabase.getInstance("https://haircutbarberdb-default-rtdb.europe-west1.firebasedatabase.app/");
+        //refCitas = database.getReference(user.getUid()).child("Citas");
+
+
+
+
 
         setSupportActionBar(binding.toolbar);
 
@@ -64,6 +91,27 @@ public class MainActivity extends AppCompatActivity  {
             return true;
         });
 
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.nav_settings:
+                        Toast.makeText(MainActivity.this, "Esto va tambien", Toast.LENGTH_SHORT).show();
+                        binding.drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_info:
+                        Toast.makeText(MainActivity.this, "Esto va tambien", Toast.LENGTH_SHORT).show();
+                        binding.drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                    case R.id.nav_login:
+                        logIn();
+                }
+
+
+                return true;
+            }
+        });
 
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
@@ -74,6 +122,42 @@ public class MainActivity extends AppCompatActivity  {
         });
 
     }
+
+    private void logIn() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View vistaDialog = LayoutInflater.from(this).inflate(R.layout.login_alert_dialog, null);
+        builder.setTitle("Iniciar Sesion");
+        builder.setView(vistaDialog);
+        builder.setCancelable(false);
+        builder.setNegativeButton("Cancelar", null);
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                TextView email = vistaDialog.findViewById(R.id.txtEmailAlertLogIn);
+                TextView password = vistaDialog.findViewById(R.id.txtPasswdAlertLogIn);
+
+                if (!email.getText().toString().isEmpty()&& !password.getText().toString().isEmpty()) {
+                    doLogIn(email,password);
+                }else{
+                    Toast.makeText(MainActivity.this, "Rellena los campos", Toast.LENGTH_SHORT).show();
+                }
+                
+
+
+            }
+
+
+        });
+        builder.create().show();
+
+
+    }
+
+    private void doLogIn(TextView email, TextView password) {
+        Toast.makeText(this, "AQUI LLEGO", Toast.LENGTH_SHORT).show();
+    }
+
+
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
