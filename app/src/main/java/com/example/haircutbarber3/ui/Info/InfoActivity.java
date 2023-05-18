bbad9d7de38fcc71ac95ec4b931036a4ed248621
Pablo <pablo.bruixola.navarro@cieep.com>
2023-05-18 17:39:44 +0200

    new features

---
+++ app/src/main/java/com/example/haircutbarber3/ui/Info/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.haircutbarber3.ui.Info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.haircutbarber3.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
